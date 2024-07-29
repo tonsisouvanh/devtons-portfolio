d59736b7406fe504a57f264cfdc1a931f42dd03c
tonsisouvanh <tonsisouvanh7@gmail.com>
2024-07-29 10:11:08 +0700

    added google analytics

--- app/page.jsx
+++ app/page.jsx
@@ -2,11 +2,13 @@ import About from "@/components/About";
 import BackgroundAnimation from "@/components/BackgroundAnimation";
 import Hero from "@/components/Hero";
 import Projects from "@/components/Projects";
+import { GoogleTagManager } from "@next/third-parties/google";
 import React from "react";
 
 const HomePage = () => {
   return (
     <>
+      <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GA} />
       <div className="min-h-screen relative flex flex-col gap-20 lg:gap-32">
         <div className="fixed top-0 left-0 right-0 -z-20 h-full w-full">
           <BackgroundAnimation />
